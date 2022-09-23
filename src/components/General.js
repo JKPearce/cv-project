c8d556e96b8d69d2791796f9391e9c41f323392b
Jaydon <pearcejaydon@gmail.com>
2022-09-23 22:23:47 +1000

    Add handleChange and add to state

--- src/components/General.js
+++ src/components/General.js
@@ -5,6 +5,16 @@ import React, { Component } from "react";
 class General extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
   }
 
   render() {
@@ -15,19 +25,32 @@ class General extends Component {
           name="firstName"
           id="firstName"
           placeholder="First Name"
+          onChange={this.handleChange}
+          value={this.state.firstName}
         />
         <input
           type="text"
           name="lastName"
           id="lastName"
           placeholder="Last Name"
+          onChange={this.handleChange}
+          value={this.state.lastName}
         />
-        <input type="email" name="email" id="email" placeholder="Email" />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+          onChange={this.handleChange}
+          value={this.state.email}
+        />
         <input
           type="tel"
           name="phoneNumber"
           id="phoneNumber"
           placeholder="Phone Number"
+          onChange={this.handleChange}
+          value={this.state.phoneNumber}
         />
       </div>
     );
