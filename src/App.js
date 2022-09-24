1923f045e17baa9726c83394b4103ed55b73cfb3
Jaydon <pearcejaydon@gmail.com>
2022-09-24 14:11:22 +1000

    Tidy up

--- src/App.js
+++ src/App.js
@@ -25,15 +25,24 @@ class App extends Component {
   handleGeneral = (data) => {
     this.setState({ general: data });
   };
+  handleEducation = (data) => {
+    this.setState({ education: data });
+  };
+  handleExperience = (data) => {
+    this.setState({ experience: data });
+  };
 
   render() {
     const { general } = this.state;
     return (
       <main>
         <div className="input-section">
+          <h2>Personal Details</h2>
           <General parentCallback={this.handleGeneral} />
-          <Education />
-          <Experience />
+          <h2>Education</h2>
+          <Education parentCallback={this.handleEducation} />
+          <h2>Experience</h2>
+          <Experience parentCallback={this.handleExperience} />
         </div>
         <CV data={general} />
       </main>
