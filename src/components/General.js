de76a8a7282c991cec2b7f22996b9d21a5c30abb
Jaydon <pearcejaydon@gmail.com>
2022-10-01 16:45:05 +1000

    Cleanup General

--- src/components/General.js
+++ src/components/General.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-useless-constructor */
 //A section to add general information like name, email, phone number.
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 const General = ({ general, onChange }) => {
   return (
@@ -49,72 +49,5 @@ const General = ({ general, onChange }) => {
     </div>
   );
 };
-
-// class General extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {};
-
-//     this.handleChange = this.handleChange.bind(this);
-//   }
-
-//   handleChange = (e) => {
-//     this.setState(
-//       {
-//         [e.target.name]: e.target.value,
-//       },
-//       () => this.props.parentCallback(this.state)
-//     );
-//   };
-
-//   render() {
-//     return (
-//       <div className="general">
-//         <input
-//           type="text"
-//           name="firstName"
-//           id="firstName"
-//           placeholder="First Name"
-//           onChange={this.handleChange}
-//           value={this.state.firstName}
-//         />
-//         <input
-//           type="text"
-//           name="lastName"
-//           id="lastName"
-//           placeholder="Last Name"
-//           onChange={this.handleChange}
-//           value={this.state.lastName}
-//         />
-//         <input
-//           type="email"
-//           name="email"
-//           id="email"
-//           placeholder="Email"
-//           onChange={this.handleChange}
-//           value={this.state.email}
-//         />
-//         <input
-//           type="tel"
-//           name="phoneNumber"
-//           id="phoneNumber"
-//           placeholder="Phone Number"
-//           onChange={this.handleChange}
-//           value={this.state.phoneNumber}
-//         />
-//         <textarea
-//           name="aboutMe"
-//           id="aboutMe"
-//           placeholder="A short description / About me"
-//           rows="3"
-//           maxLength={300}
-//           onChange={this.handleChange}
-//           value={this.state.aboutMe}
-//         />
-//       </div>
-//     );
-//   }
-// }
 
 export default General;
